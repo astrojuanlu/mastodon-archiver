d86ef09b0f68dbdb40a106dc2a305ac3aace0509
Juan Luis Cano Rodríguez <hello@juanlu.space>
2024-09-08 22:56:35 +0200

    First version of the exporter

---
+++ README.md
@@ -0,0 +1,3 @@
+# mastodon-archiver
+
+(TBC)
---
+++ generate.py
@@ -0,0 +1,68 @@
+import typing as t
+import json
+from pathlib import Path
+
+from pydantic import BaseModel, AwareDatetime, TypeAdapter
+from structlog import get_logger
+from jinja2 import FileSystemLoader, Environment
+
+logger = get_logger()
+
+type MediaType = t.Literal["video/mp4", "image/jpeg", "image/png", "audio/mpeg"]
+type Type = t.Literal["Announce", "Create"]
+
+
+class Attachment(BaseModel):
+    url: str
+    mediaType: MediaType
+    name: str | None  # alt text
+
+
+class Object(BaseModel):
+    url: str
+    content: str
+    attachment: list[Attachment]
+
+
+class Toot(BaseModel):
+    id: str
+    type: Type
+    actor: str
+    published: AwareDatetime
+    object: Object | str
+
+
+def generate_archive(
+    input_directory=Path("export"),
+    template_dir=Path("templates"),
+    base="https://social.juanlu.space/@astrojuanlu/",
+    output_directory=Path("output"),
+):
+    with (input_directory / "outbox.json").open() as fh:
+        contents = json.load(fh)
+
+    toots_adapter = TypeAdapter(list[Toot])
+    toots = toots_adapter.validate_python(contents["orderedItems"])
+
+    logger.info("Loaded toots from outbox.json", num_toots=len(toots))
+
+    jinja_env = Environment(
+        loader=FileSystemLoader(template_dir),
+        autoescape=True,
+    )
+    template = jinja_env.get_template("toot.html.j2")
+
+    output_directory.mkdir(exist_ok=True)
+
+    for toot in toots:
+        if toot.type == "Announce":
+            logger.debug("Skipping", toot=toot)
+            continue
+
+        output_path = output_directory / toot.object.url.removeprefix(base)
+        with output_path.with_suffix(".html").open("w") as fh:
+            fh.write(template.render(toot=toot))
+
+
+if __name__ == "__main__":
+    generate_archive()
